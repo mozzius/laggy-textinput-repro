85e04be1014a83fb99d993774829fff2be6bc4b3
Samuel Newman <mozzius@protonmail.com>
2024-04-19 01:38:12 +0100

    add text + autoCorrect prop

--- ReproducerApp/App.tsx
+++ ReproducerApp/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /**
  * Sample React Native App
  * https://github.com/facebook/react-native
@@ -5,28 +6,54 @@
  * @format
  */
 
-import {useState} from 'react';
-import {TextInput, View, SafeAreaView, Text} from 'react-native';
+import React, {useState} from 'react';
+import {TextInput, View, SafeAreaView, Text, Switch} from 'react-native';
 
 export default function App() {
+  const [autoCorrect, setAutoCorrect] = useState(false);
   return (
     <SafeAreaView style={{flex: 1}}>
       <View style={{padding: 20, gap: 20}}>
-        <WithValueProp />
-        <WithoutValueProp />
+        <WithValueProp
+          autoCorrect={autoCorrect}
+          key={autoCorrect ? 'a' : 'b'}
+        />
+        <WithoutValueProp
+          autoCorrect={autoCorrect}
+          key={autoCorrect ? 'c' : 'd'}
+        />
+        <Text>
+          The top input has the value prop set, the bottom one doesn't. If you spam type in the top one, the cursor will lag behind the text and you'll end up typing in the middle of a word.
+        </Text>
+        <Text>
+          Go to Settings > General > Keyboard and turn off both Auto-Correction and Predictive Text to see both work perfectly.
+        </Text>
+        <Text>
+          You can turn on/off the autoCorrect prop with the switch below, to demonstrate that it's not able to fix the issue.
+        </Text>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <Text>autoCorrect {autoCorrect ? 'enabled' : 'disabled'}</Text>
+          <Switch value={autoCorrect} onValueChange={setAutoCorrect} />
+        </View>
       </View>
     </SafeAreaView>
   );
 }
 
-function WithValueProp() {
+function WithValueProp({autoCorrect = false}) {
   const [text, setText] = useState('');
 
   return (
     <View>
       <TextInput
         style={{height: 40, fontSize: 20}}
-        placeholder="Without value prop"
+        autoCorrect={autoCorrect}
+        placeholder="With value prop"
         onChangeText={setText}
         value={text}
       />
@@ -35,13 +62,14 @@ function WithValueProp() {
   );
 }
 
-function WithoutValueProp() {
+function WithoutValueProp({autoCorrect = false}) {
   const [text, setText] = useState('');
 
   return (
     <View>
       <TextInput
         style={{height: 40, fontSize: 20}}
+        autoCorrect={autoCorrect}
         placeholder="Without value prop"
         onChangeText={setText}
       />
